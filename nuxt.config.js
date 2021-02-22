fcf1062cff0c99b36d4dfa2d03210bb0b7d80d3c
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-02-22 16:16:24 +0100

    INDENT. Format code with prettier and eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,48 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'nuxt-sanity-app',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+	// Target (https://go.nuxtjs.dev/config-target)
+	target: 'static',
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+	// Global page headers (https://go.nuxtjs.dev/config-head)
+	head: {
+		title: 'nuxt-sanity-app',
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
+			{ hid: 'description', name: 'description', content: '' }
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+	// Global CSS (https://go.nuxtjs.dev/config-css)
+	css: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+	plugins: [],
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+	// Auto import components (https://go.nuxtjs.dev/config-components)
+	components: true,
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+	buildModules: [
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+		// https://go.nuxtjs.dev/eslint
+		'@nuxtjs/eslint-module'
+	],
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
-}
+	// Modules (https://go.nuxtjs.dev/config-modules)
+	modules: [
+
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios'
+	],
+
+	// Axios module configuration (https://go.nuxtjs.dev/config-axios)
+	axios: {},
+
+	// Build Configuration (https://go.nuxtjs.dev/config-build)
+	build: {}
+};
